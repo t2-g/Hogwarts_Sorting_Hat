8389472045cf910ab46aa8a897300bdb55837e34
Tushar Sharma <75074268+tushxr16@users.noreply.github.com>
2021-10-17 15:44:45 +0530

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var questionIndex = 0;
+  var questions = [
+    [
+      'How was your trip to Azbakan?',
+    ],
+    [
+      'Which old Hogwarts relic did Harry pull out of the Sorting Hat?',
+      'Hufflepuff\'s Cup',
+      'Slytherin\'s Locket',
+      'The Sword of Gryffindor',
+      'Ravenclaw\'s Diadem'
+    ],
+    [
+      'What\'s your favorite movie?',
+    ],
+    [
+      'What\'s your favorite dish?',
+    ],
+    [
+      'What\'s your favorite spell?',
+    ],
+    [
+      'What\'s your favorite professor?',
+    ],
+  ];
   void answerQuestion() {
     setState(() {
       questionIndex = questionIndex + 1;
@@ -21,28 +59,34 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite drink?',
-      'What\'s your favorite movie?',
-      'What\'s your favorite professor?',
-      'What\'s your favorite spell?',
-      'What\'s your favorite broom?',
-    ];
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hogwarts Sorting Hat'),
-        ),
-        body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text('Hogwarts Sorting Hat'),
+      ),
+      body: Center(
+        child: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            const SizedBox(
+              height: 100,
             ),
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 4'), onPressed: answerQuestion),
+            Text(
+              questions[questionIndex][0],
+            ),
+            ElevatedButton(
+                child: Text(questions[questionIndex][0]),
+                onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text(questions[questionIndex][0]),
+                onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text(questions[questionIndex][0]),
+                onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text(questions[questionIndex][0]),
+                onPressed: answerQuestion),
           ],
         ),
       ),
