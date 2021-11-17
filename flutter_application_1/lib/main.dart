43b8bccf97fe57dcec3d34f9b81ef51b113f3aaa
t2-g <89742029+t2-g@users.noreply.github.com>
2021-11-17 23:58:12 +0530

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,94 +1,147 @@
 import 'package:flutter/material.dart';
 
+import './quiz.dart';
+import './result.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var questionIndex = 0;
-  var questions = [
-    [
-      'How was your trip to Azbakan?',
-    ],
-    [
-      'Which old Hogwarts relic did Harry pull out of the Sorting Hat?',
-      'Hufflepuff\'s Cup',
-      'Slytherin\'s Locket',
-      'The Sword of Gryffindor',
-      'Ravenclaw\'s Diadem'
-    ],
-    [
-      'What\'s your favorite movie?',
-    ],
-    [
-      'What\'s your favorite dish?',
-    ],
-    [
-      'What\'s your favorite spell?',
-    ],
-    [
-      'What\'s your favorite professor?',
-    ],
+class MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'Which is your favourite class?',
+      'answers': [
+        {
+          'text': 'Transfiguration',
+          'score': 4
+        },
+        {
+          'text': 'Potions',
+          'score': 3
+        },
+        {
+          'text': 'History of Magic',
+          'score': 2
+        },
+        {
+          'text': 'Charms',
+          'score': 1
+        },
+      ],
+    },
+    {
+      'questionText': 'If you could do one thing for the rest of your life...',
+      'answers': [
+        {
+          'text': 'Start your own business',
+          'score': 4
+        },
+        {
+          'text': 'Travel',
+          'score': 3
+        },
+        {
+          'text': 'Study',
+          'score': 2
+        },
+        {
+          'text': 'Raise a family',
+          'score': 1
+        },
+      ],
+    },
+    {
+      'questionText': 'Which of these most accurately describes your relationship with people?',
+      'answers': [
+        {
+          'text': 'I tend to wary away from people,so i don\'t have much friends',
+          'score': 2
+        },
+        {
+          'text': 'I find myselves becoming friends with people who can help me succeed',
+          'score': 3
+        },
+        {
+          'text': 'I have a few close friends that I would trust my life ',
+          'score': 4
+        },
+        {
+          'text': 'I love surrounding myselves with people- the more friends I have the merrier',
+          'score': 1
+        },
+      ],
+    },
+    {
+      'questionText': 'Your Patronus Charm would be a ...',
+      'answers': [
+        {
+          'text': 'Panther',
+          'score': 4
+        },
+        {
+          'text': 'Dragon',
+          'score': 3
+        },
+        {
+          'text': 'Fox',
+          'score': 2
+        },
+        {
+          'text': 'Hedgehog',
+          'score': 1
+        },
+      ],
+    },
   ];
-  void answerQuestion() {
+
+  var questionIndex = 0;
+  var totalScore = 0;
+
+  void resetQuiz() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      questionIndex = 0;
+      totalScore = 0;
     });
   }
 
-  @override
+  void answerQuestion(int score) {
+    totalScore = totalScore + score;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+    if (questionIndex < questions.length) {
+      print('Proceed to next Question');
+    } else {
+      print('Quiz is Over');
+    }
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        title: const Text('Hogwarts Sorting Hat'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Text(
-              questions[questionIndex][0],
-            ),
-            ElevatedButton(
-                child: Text(questions[questionIndex][0]),
-                onPressed: answerQuestion),
-            ElevatedButton(
-                child: Text(questions[questionIndex][0]),
-                onPressed: answerQuestion),
-            ElevatedButton(
-                child: Text(questions[questionIndex][0]),
-                onPressed: answerQuestion),
-            ElevatedButton(
-                child: Text(questions[questionIndex][0]),
-                onPressed: answerQuestion),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent[700],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Hogwart\'s Sorting Hat'),
+          backgroundColor: Colors.black,
         ),
+        body: questionIndex < questions.length
+            ? Quiz(
+                questions: questions,
+                answerQuestion: answerQuestion,
+                questionIndex: questionIndex,
+              )
+            : Result(totalScore, resetQuiz),
       ),
     );
   }
